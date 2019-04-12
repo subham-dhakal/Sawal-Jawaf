dc4a27f2617816db8cc1c77b0f79bce1de8f4ac4
subham-dhakal <subhamdhakal123@gmail.com>
2019-04-12 13:23:08 +0545

    Error solved when using CBV . Back to normal

--- boards/views.py
+++ boards/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import UpdateView, ListView
+from django.utils import timezone
 
 from. models import Board, Topic, Post
 from. forms import NewTopicForm, PostForm
 
 # Create your views here.
 
-def home(request):
-	''' Returns all the Board object in the homepage '''
-
-	boards = Board.objects.all()
-	return render(request,'index.html', context={'boards':boards})
-
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'index.html'
 
 def board_topics(request, pk):
-    b_detail=get_object_or_404(Board, pk=pk)
-    topics = b_detail.topics.order_by('-last_updated').annotate(replies=Count('posts') -1)
-    return render(request,'topics.html',context={'b_detail':b_detail,'topics':topics})
+    b_detail = get_object_or_404(Board, pk=pk)
+    topics = b_detail.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'b_detail': b_detail, 'topics': topics})
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'topic_posts.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        self.topic.views += 1
+        self.topic.save()
+        kwargs['topic'] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset = self.topic.posts.order_by('created_at')
+        return queryset
 
 @login_required
 def new_topic(request, pk):
@@ -27,18 +47,17 @@ def new_topic(request, pk):
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = request.user 
+            topic.starter = request.user
             topic.save()
             Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=request.user  
+                created_by=request.user
             )
-            return redirect('topic_posts', pk=pk, topic_pk=topic.pk) 
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
-
 
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
@@ -61,3 +80,23 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
--- myproject/settings.py
+++ myproject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
 
+    'django.contrib.humanize',
+
     'boards',
     'accounts',
 ]
